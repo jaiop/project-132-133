e58c7181481f14160231a13ee778a4c6113291a7
jaiop <86884090+jaiop@users.noreply.github.com>
2022-01-12 16:48:36 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+
+    function fruit_basket()
+    {
+      window.location = "fruit basket.html";
+    }
+    function bedroom()
+    {
+      window.location = "bedroom.html";
+    }
+    function bottles()
+    {
+      window.location = "bottles.html";
+    }
+    function ac_and_tv()
+    {
+      window.location = "ac and tv.html";
+    }
+    function desk()
+    {
+      window.location = "desk.html";
+    }
